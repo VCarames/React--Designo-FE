92d0920d2c7947613fc6f2d562024a52d7a4ecc5
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-06 20:00:08 -0700

    Header Component Created

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import Logo from "/src/assets/logos/logo-dark.png";
+import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__content">
+        <Link className="header__logo" to="/" aria-label="Designo - Home">
+          <img src={Logo} alt="" />
+        </Link>
+
+        <Navbar />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
