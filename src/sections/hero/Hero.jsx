4a2d8851f16dd27001d96d2b07de4ee8dbcfcad6
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-09 19:50:54 -0700

    Hero Section Created

---
+++ src/sections/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero__content">
+        <div className="hero__container">
+          <h1 className="hero__heading">
+            Award-winning custom designs and digital branding solutions
+          </h1>
+          <p className="hero__text">
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <Link className="hero__button button" to="/contact">
+            Learn More
+          </Link>
+        </div>
+
+        <img
+          className="hero__img"
+          src="src/assets/home/desktop/image-hero-phone.png"
+          alt=""
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
