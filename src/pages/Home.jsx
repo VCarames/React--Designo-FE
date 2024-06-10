22457ccb77504511ddc3cd5f185b0b0e7b3ced5c
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-10 16:18:35 -0700

    Benefit Section Added to "Home" Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header/Header";
 import Hero from "../sections/hero/Hero";
 import ProjectsHome from "../sections/projects/ProjectsHome";
+import Benefits from "../sections/benefits/Benefits";
 import CallToAction from "../components/cta/CallToAction";
 import Footer from "../components/Footer/Footer";
 
@@ -11,6 +12,7 @@ function Home() {
       <main>
         <Hero />
         <ProjectsHome />
+        <Benefits />
         <CallToAction />
       </main>
       <Footer />
