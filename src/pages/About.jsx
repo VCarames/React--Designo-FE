542abb76f5d86d19a2a32d22bfa38cd66e7d66f8
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-11 21:45:24 -0700

    Intro About Added to "About" Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/header/Header";
+import IntroAbout from "../components/intro/IntroAbout";
 import HeadquartersSection from "../components/headquarters/HeadquartersSection";
 import CallToAction from "../components/cta/CallToAction";
 import Footer from "../components/Footer/Footer";
@@ -7,10 +8,15 @@ function About() {
   return (
     <div>
       <Header />
-      <h1>About Page</h1>
-      <p>This is the "About" page component</p>
 
       <main>
+        <IntroAbout
+          imgDesktop="/src/assets/about/desktop/image-about-hero.jpg"
+          imgTablet="/src/assets/about/tablet/image-about-hero.jpg"
+          img="/src/assets/about/mobile/image-about-hero.jpg"
+          heading="About Us"
+          text="Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences."
+        />
         <HeadquartersSection />
         <CallToAction />
       </main>
