14350399caa344419b2f79fdc63374250c3c1180
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-12 21:05:42 -0700

    Whys Us Section Added to "About" Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 import Header from "../components/header/Header";
 import IntroAbout from "../components/intro/IntroAbout";
+import WhyUsOne from "../sections/whyUs/WhyUsOne";
 import HeadquartersSection from "../components/headquarters/HeadquartersSection";
+import WhyUsTwo from "../sections/whyUs/WhyUsTwo";
 import CallToAction from "../components/cta/CallToAction";
 import Footer from "../components/Footer/Footer";
 
@@ -17,7 +19,9 @@ function About() {
           heading="About Us"
           text="Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences."
         />
+        <WhyUsOne heading="Why Us: Reason One" />
         <HeadquartersSection />
+        <WhyUsTwo heading="Why Us: Reason Two" />
         <CallToAction />
       </main>
       <Footer />
