c9857151eeabd2fc6ca091b14f6868e9eef5b145
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-13 20:19:57 -0700

    Contact Us Section Added to "Contact" Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,18 @@
 import Header from "../components/header/Header";
 import HeadquartersSection from "../components/headquarters/HeadquartersSection";
 import Footer from "../components/Footer/Footer";
+import ContactUs from "../sections/contact/ContactUs";
 
 function Contact() {
   return (
     <div>
       <Header />
-      <h1>Contact Page</h1>
-      <p>This is the "Contact" page component</p>
 
       <main>
+        <ContactUs
+          heading="Contact Us"
+          text="Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line."
+        />
         <HeadquartersSection />
       </main>
       <Footer />
