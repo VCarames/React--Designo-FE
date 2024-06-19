45262df8cce814add96d3fb29e68db7f72fdc3bb
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-18 20:25:56 -0700

    Hero Section Styled - SCSS [UPDATE]

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -5,14 +5,16 @@ function Hero() {
     <section className="hero">
       <div className="hero__content">
         <div className="hero__container">
-          <h1 className="hero__heading">
-            Award-winning custom designs and digital branding solutions
-          </h1>
-          <p className="hero__text">
-            With over 10 years in the industry, we are experienced in creating
-            fully responsive websites, app design, and engaging brand
-            experiences. Find out more about our services.
-          </p>
+          <div className="hero__container--text">
+            <h1 className="hero__heading">
+              Award-winning custom designs and digital branding solutions
+            </h1>
+            <p className="hero__text">
+              With over 10 years in the industry, we are experienced in creating
+              fully responsive websites, app design, and engaging brand
+              experiences. Find out more about our services.
+            </p>
+          </div>
           <Link className="hero__button button" to="/contact">
             Learn More
           </Link>
