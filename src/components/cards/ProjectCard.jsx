4883d0c219effbca2ec039b565f14eac3d361d38
VCarames <109115310+VCarames@users.noreply.github.com>
2024-06-19 22:25:14 -0700

    Project Card & Project Section Styled - SCSS [UPDATE]

--- src/components/cards/ProjectCard.jsx
+++ src/components/cards/ProjectCard.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import arrow from "/src/assets/icons/icon-right-arrow.svg";
 
-function ProjectCard({ projectCardClass, heading, link }) {
+function ProjectCard({ projectCardClass, link, project }) {
   return (
     <li className={`project-card ${projectCardClass}`} role="listitem">
-      <h3 className="project-card__heading">{heading}</h3>
       <Link className="project-card__link" to={link}>
-        <p className="project-card__link--text">View Projects</p>
-        <img className="project-card__link--img" src={arrow} alt="" />
+        <p className="project-card__project">{project}</p>
+        <div className="project-card__container">
+          <p className="project-card__text">View Projects</p>
+          <img className="project-card__img" src={arrow} alt="" />
+        </div>
       </Link>
     </li>
   );
